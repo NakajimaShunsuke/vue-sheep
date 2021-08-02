f50765d8b518dfcd25def2f9ea965a47fb9a1f06
Nakajima Shunsuke <shunsuke.skillup@gmail.com>
2021-08-02 09:42:52 +0900

    cardデザイン作成途中

---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="rounded border overflow-hidden w-64">
+		<img class="w-full" src="https://placehold.jp/500x350.png" alt="" />
+	</div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+</script>

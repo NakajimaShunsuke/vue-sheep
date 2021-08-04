5f4cb9426883575db8634ad82847b2084934f6d7
Nakajima Shunsuke <shunsuke.skillup@gmail.com>
2021-08-04 23:53:08 +0900

    画像コンポーネント完成

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -1,6 +1,18 @@
 <template>
-<img class="filter grayscale" src="../assets/images/main-img.jpg" alt="">
+	<img
+		class="block"
+		:class="{ filter: isFilter, grayscale: isGrayscale }"
+		:src="imgItem"
+		:alt="altText"
+	/>
 </template>
 
-<script setup>
+<script setup="props">
+import { defineProps } from "vue";
+defineProps({
+	imgItem: { type: String, default: "" },
+	altText: { type: String, default: "WEB-Image" },
+	isFilter: { type: Boolean, default: true },
+	isGrayscale: { type: Boolean, default: true },
+});
 </script>

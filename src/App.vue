5ecfb618085f82d4c8e6302643134759463ec427
Nakajima Shunsuke <shunsuke.nakajima@e-tenki.co.jp>
2021-08-04 13:27:43 +0900

    ワイドの設定を追加

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     lg(min-width: 1024px):green
     xl(min-width: 1280px):blue
   -->
-    <article class="bg-gray-200 sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200">
+    <article class="max-w-7xl w-full bg-gray-200 sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200">
       <img class="m-auto" alt="Vue logo" src="./assets/logo.png" />
       <HelloWorld msg="Hello Vue 3 + Vite" />
     </article>
@@ -14,7 +14,6 @@
 
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
-import Card from "./components/Card.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
@@ -28,5 +27,7 @@ import Card from "./components/Card.vue";
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+  display: flex;
+  justify-content: center;
 }
 </style>

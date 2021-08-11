851291163d0b6c6450ea8c2ecb796ffb97fcf86a
Nakajima Shunsuke <shunsuke.nakajima@e-tenki.co.jp>
2021-08-11 17:58:33 +0900

    TOP画面レイアウト仮設置

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,12 +1,12 @@
 <template>
 	<h2 v-if="heading_type === 'h2'" class="
-		m-2
+		my-8
 		text-left
-		text-2xl
-		sm:text-2xl
-		md:text-2xl
-		lg:text-3xl
-		xl:text-3xl">
+		text-3xl
+		sm:text-3xl
+		md:text-3xl
+		lg:text-4xl
+		xl:text-4xl">
 	{{ heading }}
 	</h2>
 
@@ -17,17 +17,18 @@
 		sm:text-3xl
 		md:text-4xl
 		lg:text-5xl
-		xl:text-6xl">{{ heading }}
+		xl:text-6xl"
+	>{{ heading }}
 		<span class="
-		flex
-		items-center
-		uppercase
-		text-xl
-		sm:text-2xl
-		md:text-2xl
-		lg:text-3xl
-		xl:text-3xl
-	">{{ heading_span }}</span></h1>
+			flex
+			items-center
+			uppercase
+			text-xl
+			sm:text-2xl
+			md:text-2xl
+			lg:text-3xl
+			xl:text-3xl
+		">{{ heading_span }}</span></h1>
 </template>
 
 <script setup="props">
---
+++ src/components/TastingSection.vue
@@ -0,0 +1,59 @@
+<template>
+		<section class="my-16">
+			<Heading
+				class="text-right"
+				heading_type="h2"
+				:heading="props_heading"
+			/>
+			<div class="
+				grid
+				gap-4
+				grid-cols-3
+			">
+				<div class="
+					flex flex-col
+					col-span-full
+					sm:col-span-full
+					md:col-span-2
+					lg:col-span-2
+					xl:col-span-2
+				">
+					<p v-for="text in props_text" :key="text"
+						class="
+							my-1.5
+							text-xl
+							sm:text-xl
+							md:text-xl
+							lg:text-2xl
+							xl:text-2xl
+							text-left
+						"
+					>
+						{{ text }}
+					</p>
+				</div>
+				<div class="
+					col-span-1
+					hidden
+					sm:hidden
+					md:block
+					lg:block
+					xl:block
+				"><ImageBox :imgItem="props_image" /></div>
+			</div>
+		</section>
+</template>
+<script setup="props">
+import Heading from "./Heading.vue";
+import ImageBox from "./ImageBox.vue";
+
+import { defineProps } from "vue";
+defineProps({
+  props_text: {
+    type: Array,
+    default: () => []
+  },
+  props_heading: { type: String, default: "Alcohol may be man’s worst enemy,but the bible says love your enemy." },
+  props_image: { type: String, default:"" }
+})
+</script>

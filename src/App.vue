31c6765b72d59f5711b9c72d977ac8caba0aa2d3
Nakajima Shunsuke <shunsuke.nakajima@e-tenki.co.jp>
2021-08-19 17:38:24 +0900

    プロジェクト整理 今後は目的ごとにbranchを切って作業する事

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view/>
+  <router-view class="base-layout responsive-tools" />
 </template>
 
 <script setup>
@@ -12,6 +12,34 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Shippori+Mincho&display=swap');
 
+/**
+ * This injects Tailwind's base styles and any base styles registered by
+ * plugins.
+ */
+@tailwind base;
+
+/**
+ * This injects Tailwind's component classes and any component classes
+ * registered by plugins.
+ */
+@tailwind components;
+
+/**
+ * This injects Tailwind's utility classes and any utility classes registered
+ * by plugins.
+ */
+@tailwind utilities;
+
+/**
+ * Use this directive to control where Tailwind injects the responsive
+ * variations of each utility.
+ *
+ * If omitted, Tailwind will append these classes to the very end of
+ * your stylesheet by default.
+ */
+ @tailwind screens;
+
+
 #app {
 
 	font-family: "Noto Serif JP","游明朝","Yu Mincho",YuMincho,"Hiragino Mincho ProN W3","ヒラギノ明朝 ProN W3","Hiragino Mincho ProN","HG明朝E","ＭＳ Ｐ明朝","ＭＳ 明朝",serif;
@@ -22,5 +50,19 @@
 	margin-top: 60px;
   display: flex;
   justify-content: center;
+}
+
+.responsive-tools {
+    /**
+			all(mobile): bg-gray-200
+			sm(min-width: 640px):red
+			md(min-width: 768px):yellow
+			lg(min-width: 1024px):green
+			xl(min-width: 1280px):blue
+		*/
+	@apply bg-gray-200 sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200;
+}
+.base-layout {
+	@apply max-w-7xl w-full p-2.5;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,16 @@
 <template>
-	<!--
-    all(mobile): bg-gray-200
-    sm(min-width: 640px):red
-    md(min-width: 768px):yellow
-    lg(min-width: 1024px):green
-    xl(min-width: 1280px):blue
-	-->
-	<article
-		class="
-			max-w-7xl
-			w-full
-			p-2.5
-			bg-gray-200
-			sm:bg-red-200
-			md:bg-yellow-200
-			lg:bg-green-200
-			xl:bg-blue-200
-		"
-	>
-		<!-- Maineセクション -->
-		<section>
-			<h1 class="text-2xl lg:text-4xl mb-2">
-				Civilization begins with distillation.
-			</h1>
-			<p class="text-xl lg:text-2xl mb-2 font-bold">文明は蒸留から始まる</p>
-			<p class="text-xl lg:text-lg">
-				限られた人生でお酒と過ごす時間を私は後悔していません<br />
-				色・香り・味をその一滴に感じましょう<br />
-				私の味わったお酒を皆様にお届けいたします
-			</p>
-		</section>
-		<h2 class="heading08"><span>Recruit</span>英字とラインのCSS見出し</h2>
+	<article>
+		<HelloWorld />
 	</article>
 </template>
 
 <script setup>
-import ImageBox from "../components/ImageBox.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style scoped>
-.heading08 {
-	font-size: 26px;
-}
 
-.heading08 span {
-	display: flex;
-	align-items: center;
-	color: #2c3e50;
-	font-size: 18px;
-	text-transform: uppercase;
-}
-
-.heading08 span::before {
-	content: "";
-	display: inline-block;
-	margin-right: 20px;
-	width: 40px;
-	height: 1px;
-	background-color: #2c3e50;
-}
 </style>

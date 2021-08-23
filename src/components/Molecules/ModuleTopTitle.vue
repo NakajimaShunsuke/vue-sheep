82186ac96e0180ddd2769b92791bc21d542b2546
Nakajima Shunsuke <shunsuke.nakajima@e-tenki.co.jp>
2021-08-23 13:46:55 +0900

    既に作成しているファイルのプロジェクトをリネーム

--- src/components/Molecules/ModuleTopTitle.vue
+++ src/components/Molecules/ModuleTopTitle.vue
@@ -4,8 +4,8 @@
 </template>
 
 <script setup>
-import TitleH1 from "../Atoms/TitleH1.vue";
-import TitleH2 from "../Atoms/TitleH2.vue";
+import TitleH1 from "../Atoms/AtomTitleH1.vue";
+import TitleH2 from "../Atoms/AtomTitleH2.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
